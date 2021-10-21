e921852c46f4046b09c7a81ccc5d1ee6392794ee
wistbean <wistbean.bb@gmail.com>
2021-10-21 23:59:40 +0800

    fix dangdang_top_500

--- dangdang_top_500.py
+++ dangdang_top_500.py
@@ -8,20 +8,20 @@ def request_dandan(url):
         response = requests.get(url)
         if response.status_code == 200:
             return response.text
-    except requests.RequestException:
+    except requests.RequestException as e:
+        print(e)
         return None
 
 
 def parse_result(html):
     pattern = re.compile(
-        '<li>.*?list_num.*?(\d+).</div>.*?<img src="(.*?)".*?class="name".*?title="(.*?)">.*?class="star">.*?class="tuijian">(.*?)</span>.*?class="publisher_info">.*?target="_blank">(.*?)</a>.*?class="biaosheng">.*?<span>(.*?)</span></div>.*?<p><span\sclass="price_n">¥(.*?)</span>.*?</li>',
-        re.S)
+        '<li.*?list_num.*?(\d+)\.</div>.*?<img src="(.*?)".*?class="name".*?title="(.*?)">.*?class="star">.*?class="tuijian">(.*?)</span>.*?class="publisher_info">.*?target="_blank">(.*?)</a>.*?class="biaosheng">.*?<span>(.*?)</span></div>.*?<p><span class="price_n">(.*?)</span>.*?</li>', re.S)
     items = re.findall(pattern, html)
 
     for item in items:
         yield {
             'range': item[0],
-            'iamge': item[1],
+            'image': item[1],
             'title': item[2],
             'recommend': item[3],
             'author': item[4],
